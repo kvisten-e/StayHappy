fe53b68885c54019340e66da5dabd14bb3fbee24
kvisten-e <kvist.emil@gmail.com>
2024-08-19 08:33:49 +0200

    .

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-## Test
+# Stay Happy
+
+### Install Virtual Environment
+
+1. **Navigate to the Project Directory**:
+    ```bash
+    cd StayHappyWebApp
+    ```
+
+2. **Create a Virtual Environment**:
+    ```bash
+    python -m venv .venv
+    ```
+
+3. **Activate the Virtual Environment**:
+    - On **Windows**:
+        ```bash
+        .venv\Scripts\activate
+        ```
+    - On **macOS/Linux**:
+        ```bash
+        source .venv/bin/activate
+        ```
+
+4. **Install the Required Packages**:
+    ```bash
+    pip install -r requirements.txt
+    ```
--- StayHappyWebApp/obj/Debug/net8.0/StayHappyWebApp.AssemblyInfo.cs
+++ StayHappyWebApp/obj/Debug/net8.0/StayHappyWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StayHappyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8172920e526bb727f8e061039ce11a908bb35a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d882ccf170f4e518f2842ba78b3e20941a56d4fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("StayHappyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StayHappyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
