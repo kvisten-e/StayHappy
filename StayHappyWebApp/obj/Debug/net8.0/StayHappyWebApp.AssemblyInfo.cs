08fc74e48f42051895bb8d0bfb922f8abbc97e33
kvisten-e <kvist.emil@gmail.com>
2024-08-19 18:10:39 +0200

    .

--- StayHappyWebApp/obj/Debug/net8.0/StayHappyWebApp.AssemblyInfo.cs
+++ StayHappyWebApp/obj/Debug/net8.0/StayHappyWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StayHappyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d271d2e8d457a835ee8e089aac97d32739a7c260")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f6bb6c94b0eb54eb8132ef916ceccd5f6b2e338")]
 [assembly: System.Reflection.AssemblyProductAttribute("StayHappyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StayHappyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
